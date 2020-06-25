383380f89cce0b39f0c82e4d40051d3274216ad6
Renzo Pederzoli <rdp73@miami.edu>
2020-06-25 18:26:32 -0400

    loot sys added

--- JS/loot.js
+++ JS/loot.js
@@ -1,14 +1,24 @@
 //Might think about adding new arrays for stronger mosnter
-let lootArray = [
+const commonLoot = [
   { name: "Wood Sword", atk: 5, def: 0, health: 0, rarity: "Common"},
-  { name: "Wood Sword", atk: 5, def: 0, health: 0, rarity: "Common"},
-  { name: "Paper Armor", atk: 0, def: 2.5, health: 0, rarity: "Common"},
-  { name: "Paper Armor", atk: 0, def: 2.5, health: 0, rarity: "Common"},
+  { name: "Paper Armor", atk: 0, def: 2.5, health: 0, rarity: "Common"}
+]
 
-  { name: "Puny Heal", atk: 0, def: 0, health: 25, rarity: "Uncommon"},
+const uncommonLoot = [
+  { name: "Puny Heal", atk: 0, def: 0, health: 25, rarity: "Uncommon"}
+]
 
-  { name: "Felipe's Rod", atk: 50, def: 20, health: 40, rarity: "Legendary"}
-  // { name: "Zeek", atk: 350000, def: 11, health: 0, rarity: "Legendary"},
-  // { name: "Angel's Lightning", atk: 25, def: 50, health: 0, rarity: "Legendary"},
-  // { name: "Hezi's Chopsticks", atk: 100000, def: 95, health: 25, rarity: "Legendary"}
-];
+const rareLoot = [
+  { name: "Steel sword", atk: 15, def: 0, health: 0, rarity: "Rare"}
+]
+
+const epicLoot = [
+
+]
+
+const legendaryLoot = [
+  { name: "Felipe's Rod", atk: 50, def: 20, health: 40, rarity: "Legendary"},
+  { name: "Zeek", atk: 350000, def: 11, health: 0, rarity: "Legendary"},
+  { name: "Angel's Lightning", atk: 25, def: 50, health: 0, rarity: "Legendary"},
+  { name: "Hezi's Chopsticks", atk: 100000, def: 95, health: 25, rarity: "Legendary"}
+]
--- JS/monster.js
+++ JS/monster.js
@@ -12,50 +12,65 @@ class Monster {
       this.health = Math.round(Math.random() * (120 - 80)) + 80
       this.power = Math.round(Math.random() * (15 - 5)) + 5 
       this.defense = Math.round(Math.random() * (5))
-      this.loot = this.generateRandomLoot(lootArray)
+      this.loot = this.generateRandomLoot()
     }
     else if (killCount >= 6 && killCount <= 10) {
       this.health = Math.round(Math.random() * (140 - 100)) + 100
       this.power = Math.round(Math.random() * (20 - 10)) + 10
       this.defense = Math.round(Math.random() * (10))
       this.tier = 4
-      this.loot = this.generateRandomLoot(lootArray)
+      this.loot = this.generateRandomLoot()
     }
     else if (killCount >= 11 && killCount <= 15) {
       this.health = Math.round(Math.random() * (160 - 120)) + 120
       this.power = Math.round(Math.random() * (25 - 15)) + 15
       this.defense = Math.round(Math.random() * (15))
       this.tier = 3
-      this.loot = this.generateRandomLoot(lootArray)
+      this.loot = this.generateRandomLoot()
     }
     else if (killCount >= 16 && killCount <= 20) {
       this.health = Math.round(Math.random() * (180 - 140)) + 140
       this.power = Math.round(Math.random() * (30 - 20)) + 20
       this.defense = Math.round(Math.random() * (20))
       this.tier = 2
-      this.loot = this.generateRandomLoot(lootArray)
+      this.loot = this.generateRandomLoot()
     }
     else {
       this.health = Math.round(Math.random() * (200 - 160)) + 160
       this.power = Math.round(Math.random() * (35 - 25)) + 25
       this.defense = Math.round(Math.random() * (25))
       this.tier = 1
-      this.loot = this.generateRandomLoot(lootArray)
+      this.loot = this.generateRandomLoot()
+    }
+  }
+
+  determineLootPool() {
+    if (this.tier === 5) {
+      let rand = Math.random()
+      if (rand < 0.7)
+        return commonLoot
+      else if (rand >= 0.7 && rand < 0.98)
+        return uncommonLoot
+      else 
+        return rareLoot
     }
   }
 
   // Grabs a random loot from the loot.js file
-  generateRandomLoot(arr) {
+  generateRandomLoot() {
     let itemArray = []
 
+    let arr = this.determineLootPool()
     let randomIndex = Math.floor(Math.random() * arr.length)
     let randomItem1 = arr[randomIndex]
     itemArray.push(randomItem1)
 
+    arr = this.determineLootPool()
     randomIndex = Math.floor(Math.random() * arr.length)
     let randomItem2 = arr[randomIndex]
     itemArray.push(randomItem2)
 
+    arr =this.determineLootPool()
     randomIndex = Math.floor(Math.random() * arr.length)
     let randomItem3 = arr[randomIndex]
     itemArray.push(randomItem3)
