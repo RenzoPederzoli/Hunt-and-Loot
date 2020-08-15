89eb319012053875fbf9ce4527076695c2b1957a
Renzop9 <51340433+renzop9@users.noreply.github.com>
2020-08-15 15:49:25 -0400

    added Unlucky item

--- JS/loot.js
+++ JS/loot.js
@@ -36,7 +36,8 @@ const legendaryLoot = [
   { name: "Jayson's Bow", atk: 500, def: 0, health: 0, rarity: "Legendary"},
   { name: "Gustavo's Brain", atk: 0, def: 0, health: 400, rarity: "Legendary"},
   { name: "Ahal's Bongos", atk: 350, def: 0, health: 40, rarity: "Legendary"},
-  { name: "Juan's Resurrection", atk: 0, def: 0, health: 0, rarity: "Legendary"} 
+  { name: "Juan's Resurrection", atk: 0, def: 0, health: 0, rarity: "Legendary"} ,
+  { name: "Unlucky", atk: 0, def: 0, health: -500, rarity: "Legendary"} 
 ]
 
 const bossLoot = [
